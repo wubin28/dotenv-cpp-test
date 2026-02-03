3b9b852040a36fc7a358265b52472b299c7eb7ce
wubin28 <wubinben@icloud.com>
2026-02-03 22:48:53 +0800

    feat: finished test 1 part 2 in dotenv-cpp-windows-guide.md

--- example.cpp
+++ example.cpp
@@ -1,21 +1,11 @@
-// example.cpp
 #include<iostream>
 #include"dotenv.h"
 
 int main()
 {
-    // 初始化dotenv，加载.env文件
     dotenv::init();
-
-    // 读取环境变量并输出
-    std::cout << "DATABASE_HOST: " << std::getenv("DATABASE_HOST") << std::endl;
-    std::cout << "DATABASE_USERNAME: " << std::getenv("DATABASE_USERNAME") << std::endl;
-    std::cout << "DATABASE_PASSWORD: " << std::getenv("DATABASE_PASSWORD") << std::endl;
-
-    // 使用dotenv::getenv()的默认值功能
-    std::cout << "DATABASE_PORT: "
-              << dotenv::getenv("DATABASE_PORT", "3306")
-              << std::endl;
-
+    std::cout << "BASE_PATH: " << std::getenv("BASE_PATH") << std::endl;
+    std::cout << "LOG_PATH: " << std::getenv("LOG_PATH") << std::endl;
+    std::cout << "CONFIG_PATH: " << std::getenv("CONFIG_PATH") << std::endl;
     return 0;
 }
